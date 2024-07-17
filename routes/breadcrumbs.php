8ca4bf954f1b2fe55b9cbe3658a12f1cff5a7b94
Alex Garrett-Smith <alex@codecourse.com>
2024-07-17 09:17:48 +0100

    Episode 07

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -2,7 +2,19 @@
 
 use Diglactic\Breadcrumbs\Breadcrumbs;
 use Diglactic\Breadcrumbs\Generator as BreadcrumbTrail;
+use App\Models\Category;
 
 Breadcrumbs::for('dashboard', function (BreadcrumbTrail $trail) {
     $trail->push('Dashboard', route('dashboard'));
+});
+
+Breadcrumbs::for('categories', function (BreadcrumbTrail $trail, ?Category $category = null) {
+    $trail->parent('dashboard');
+    $trail->push('Categories', route('categories'));
+
+    if ($category) {
+        foreach ($category->ancestorsAndSelf($category->id) as $ancestor) {
+            $trail->push($ancestor->title, route('categories.show', $ancestor));
+        }
+    }
 });
